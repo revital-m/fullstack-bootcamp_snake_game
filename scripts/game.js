1a98964f8f54a164dcf9f35f5de85f1923bf34a5
revital-m <revital13m@gmail.com>
2021-12-01 11:25:44 +0200

    added the snake body & movement

---
+++ scripts/game.js
@@ -0,0 +1,28 @@
+import { update as updateSanke, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
+let lastRenderTime = 0;
+const gameBoard = document.querySelector('#game-board');
+
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) {return;}
+
+//   console.log('Render');
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update(){
+    updateSanke();
+}
+
+function draw(){
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+}
